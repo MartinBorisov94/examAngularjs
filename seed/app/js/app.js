1dfd3661e85baaf4be450400b79af30fa20820b3
MartinBorisov94 <martinborisov94@gmail.com>
2015-01-01 03:18:29 +0200

    ngRout working

--- seed/app/js/app.js
+++ seed/app/js/app.js
@@ -2,9 +2,17 @@
 
 var app = angular.module('softUniApp', ['ngResource', 'ngRoute'])
     .config(function ($routeProvider) {
-        $routeProvider.when('/ads', {
+        $routeProvider.when('/', {
             templateUrl: 'templates/allAds.html',
             controller: 'AllAdsController'
+        });
+        $routeProvider.when('/login', {
+           templateUrl: 'templates/login.html'
+            /*TODO add controller*/
+        });
+        $routeProvider.when('/register', {
+            templateUrl: 'templates/register.html'
+            /*TODO add controller*/
         });
     });
 
